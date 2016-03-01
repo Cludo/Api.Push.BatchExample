e06f7fd5ff0f59f24ad528d75e261bc370fb87c0
unknown <vob@nm-vod.nmester.dk>
2016-03-01 11:55:24 +0100

    delte data

--- BatchApiExample/BatchApiExample/Program.cs
+++ BatchApiExample/BatchApiExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -29,9 +30,10 @@ namespace BatchApiExample
             }
             var customerId = -1; // YOUR_CUSTOMER_ID
             var contentId = -1; // YOUR_CUSTOMER_ID       
-            var customerKey = "CUSTOMER_KEY"; //Customer key
+            var customerKey = "CUSTOMER_KEY_GOES_HERE"; //Customer key
             var api = new CludoApi(customerId, customerKey);
             api.PushData(contentId, data);
+            api.DeleteData(contentId, data.Select(d=> new KeyValuePair<string, string>(d["Id"].ToString(), d["Type"].ToString())));
         }
 
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
@@ -41,7 +43,7 @@ namespace BatchApiExample
 
     public class CludoApi
     {
-        private const string CludoSearchApiUrl = "http://api.cludo.com/"; // Add this url to your config file as well
+        private const string CludoSearchApiUrl = "https://api.cludo.com/"; 
         private readonly int _customerId;
 
         private readonly string _customerKey;
@@ -62,6 +64,23 @@ namespace BatchApiExample
                 Convert.ToBase64String(Encoding.UTF8.GetBytes(_customerId + ":" + _customerKey)));
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             return client;
+        }
+
+        public void DeleteData(int contentId, IEnumerable<KeyValuePair<string, string>> data)
+        {
+            using (var client = GetClient())
+            {
+                var requestUrl = string.Format("/api/v3/{0}/content/{1}/delete", _customerId, contentId);
+                var result = client.PostAsync(requestUrl,
+                    new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")).Result;
+                if (!result.IsSuccessStatusCode)
+                {
+                    if (result.StatusCode == HttpStatusCode.BadRequest)
+                    {
+                        //TODO: handle invalid objects List<KeyValuePair<Dictionary<string, object>, string>>
+                    }
+                }
+            }
         }
 
         public void PushData(int contentId, List<Dictionary<string, object>> data)
