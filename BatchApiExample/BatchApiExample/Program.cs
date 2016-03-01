1d21a9bd035d60db44f95eb29408a2932f03a7b2
unknown <vob@nm-vod.nmester.dk>
2016-03-01 10:20:20 +0100

    Fix api versioning

--- BatchApiExample/BatchApiExample/Program.cs
+++ BatchApiExample/BatchApiExample/Program.cs
@@ -1,57 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web.Configuration;
+using Faker;
 using Newtonsoft.Json;
 
 namespace BatchApiExample
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-           var data = new List<Dictionary<string, object>>();
+            var data = new List<Dictionary<string, object>>();
             for (var i = 0; i < 10; i++)
             {
                 var obj = new Dictionary<string, object>
                 {
                     ["Id"] = Guid.NewGuid(),
                     ["Type"] = "PageContent",
-                    ["Title"] = Faker.Lorem.Sentence(),
-                    ["Description"] = Faker.Lorem.Sentence(),
-                    ["Array"] = new[] {Faker.Lorem.Sentence(), Faker.Lorem.Sentence(), Faker.Lorem.Sentence()}
+                    ["Title"] = Lorem.Sentence(),
+                    ["Description"] = Lorem.Sentence(),
+                    ["Array"] = new[] {Lorem.Sentence(), Lorem.Sentence(), Lorem.Sentence()}
                 };
                 data.Add(obj);
             }
             var customerId = -1; // YOUR_CUSTOMER_ID
-            var contentId = -1; // YOUR_CUSTOMER_ID                
-            var api = new CludoApi(customerId);
+            var contentId = -1; // YOUR_CUSTOMER_ID       
+            var customerKey = "CUSTOMER_KEY"; //Customer key
+            var api = new CludoApi(customerId, customerKey);
             api.PushData(contentId, data);
         }
 
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            
         }
     }
 
     public class CludoApi
     {
+        private const string CludoSearchApiUrl = "http://api.cludo.com/"; // Add this url to your config file as well
         private readonly int _customerId;
 
-        public CludoApi(int customerId)
-        {
-            this._customerId = customerId;
-        }
+        private readonly string _customerKey;
 
-        private const string CludoSearchApiUrl = "http://search.local/"; // Add this url to your config file as well
+        public CludoApi(int customerId, string customerKey)
+        {
+            _customerId = customerId;
+            _customerKey = customerKey;
+        }
 
         private HttpClient GetClient()
         {
@@ -60,35 +59,25 @@ namespace BatchApiExample
                 BaseAddress = new Uri(CludoSearchApiUrl)
             };
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                Convert.ToBase64String(Encoding.UTF8.GetBytes(_customerId + ":" + CustomerKey)));
+                Convert.ToBase64String(Encoding.UTF8.GetBytes(_customerId + ":" + _customerKey)));
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             return client;
-        }
-
-        private string CustomerKey
-        {
-            get { return ConfigurationManager.AppSettings["Cludo.CustomerKey"]; }
         }
 
         public void PushData(int contentId, List<Dictionary<string, object>> data)
         {
             using (var client = GetClient())
             {
-                var requestUrl = string.Format("/api/{0}/content/{1}/push", _customerId, contentId);
+                var requestUrl = string.Format("/api/v3/{0}/content/{1}/push", _customerId, contentId);
                 var result = client.PostAsync(requestUrl,
-                        new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")).Result;
+                    new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")).Result;
                 if (!result.IsSuccessStatusCode)
                 {
                     if (result.StatusCode == HttpStatusCode.BadRequest)
                     {
                         //TODO: handle invalid objects List<KeyValuePair<Dictionary<string, object>, string>>
                     }
-                    else
-                    {
-                        //TODO: log exeption
-                    }
                 }
-                
             }
         }
     }
